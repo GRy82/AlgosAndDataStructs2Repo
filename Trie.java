68f29b342b74893a12016760d1640af60b0c7de7
GRy82 <gwryan@buffalo.edu>
2021-03-15 15:48:11 -0400

    Use of array as underlying structure in Trie class has been changed to a HashMap so as to save space in memory.

--- Trie.java
+++ Trie.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
+
 public class Trie {
     
     private class Node{
         public  int ALPHABET_SIZE = 26;
         private boolean endOfWord;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private char value;
 
         public Node(char value){
@@ -20,10 +22,9 @@ public class Trie {
     public void insert(String word){
         Node current = root;
         for(var ch : word.toCharArray()){
-            int index = ch - 'a';
-            if(current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if(current.children.get(ch) == null)
+                current.children.put(ch, new Node(ch));
+            current = current.children.get(ch);
         }
         current.endOfWord = true;
     }
