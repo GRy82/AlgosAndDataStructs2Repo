64027093f06d9fc66c5f30e72e0a12b635153dbe
GRy82 <gwryan@buffalo.edu>
2021-03-22 20:55:37 -0400

    getShortestDistance method implemented. It uses a priority queue to store shortest distances, and for each element in the queue, it will find all neighbors to get the next shortest distance.

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -2,15 +2,14 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class WeightedGraph {
     private Map<String, Node> entities = new HashMap<>();
-
-    private Map<Node, Integer> distances = new HashMap<>();
-    private Map<Node, Node> previousNodes = new HashMap<>();
 
     private class Node{
         private String label;
@@ -71,5 +70,43 @@ public class WeightedGraph {
         }
     }
 
+    private class NodeEntry{
+        private Node node;
+        private int priority;
+
+        public NodeEntry(Node node, int priority){
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
+    public int getShortestDistance(String from, String to){
+        var fromNode = entities.get(from);
+        Map<Node, Integer> distances = new HashMap<>();
+        for(var node : entities.values())
+            distances.put(node, Integer.MAX_VALUE);
+        distances.replace(fromNode, 0);
+
+        Set<Node> visited = new HashSet<>();
+
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(
+            Comparator.comparingInt(ne -> ne.priority));
     
+        queue.add(new NodeEntry(fromNode, 0));
+
+        while(!queue.isEmpty()){
+            var currentNode = queue.remove().node;
+            visited.add(currentNode);
+            for(var edge : currentNode.getEdges()){
+                if(visited.contains(edge.to)) continue;
+                
+                var newDistance = distances.get(currentNode) + edge.weight;
+                if (newDistance < distances.get(edge.to)){
+                    distances.replace(edge.to, newDistance);
+                    queue.add(new NodeEntry(edge.to, newDistance));
+                }
+            }
+        }
+        return distances.get(entities.get(to));
+    }
 }
